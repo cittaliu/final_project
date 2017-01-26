cb16a866deec5963306da3860738f00dc1148a51
sophieluo <sophie@groobusiness.com>
2017-01-26 00:17:34 -0800

    last commit

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :company
 
-  validates :first_name, uniqueness: true
   has_many :usercontacts, dependent: :destroy
   has_many :users, through: :usercontacts
 
